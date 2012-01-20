810e956c93132b2815408930d44a43145dc8227c
Steven Haryanto <stevenharyanto@gmail.com>
2012-01-20 03:54:23 +0000

    Import of SHARYANTO/Sub-Spec-Gen-ForModule-0.02 from CPAN.
    gitpan-cpan-distribution: Sub-Spec-Gen-ForModule
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/Sub-Spec-Gen-ForModule-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Sub/Spec/Gen/ForModule.pm
+++ lib/Sub/Spec/Gen/ForModule.pm
@@ -1,6 +1,6 @@
 package Sub::Spec::Gen::ForModule;
-BEGIN {
-  $Sub::Spec::Gen::ForModule::VERSION = '0.01';
+{
+  $Sub::Spec::Gen::ForModule::VERSION = '0.02';
 }
 
 use 5.010;
@@ -126,7 +126,7 @@ Sub::Spec::Gen::ForModule - Generate spec for subroutines in a module
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
@@ -145,6 +145,12 @@ In another script:
 Now Foo's functions have sub specs (in %Foo::SPEC).
 
 =head1 DESCRIPTION
+
+B<NOTICE>: This module and the L<Sub::Spec> standard is deprecated as of Jan
+2012. L<Rinci> is the new specification to replace Sub::Spec, it is about 95%
+compatible with Sub::Spec, but corrects a few issues and is more generic.
+C<Perinci::*> is the Perl implementation for Rinci and many of its modules can
+handle existing Sub::Spec sub specs.
 
 This module provides gen_spec_for_module().
 
@@ -208,7 +214,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Steven Haryanto.
+This software is copyright (c) 2012 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
